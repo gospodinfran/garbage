732121c3732bec985de4e2e13f321f8ee70263c3
Fran <kezicfran@gmail.com>
2022-12-25 11:30:16 +0200

    added functionality

--- todoapp/src/Greet.jsx
+++ todoapp/src/Greet.jsx
@@ -1,7 +1,8 @@
 export default function Greet(props) {
     return (<>
     <label htmlFor='name' >Your name: </label>      <br />
-    <input type='text' value={props.name} onChange={props.onNameChange} />
+    <input type='text' value={props.name} onChange={props.onNameChange}
+    autoFocus />
     <p>Hi {props.name}!</p>
     </>)
 }
